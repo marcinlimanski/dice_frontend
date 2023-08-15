8d0f2c03d54f0347f7570a2baf305601f4c576ef
Marcin Limanski <marcinlimanski@outlook.com>
2023-08-15 14:17:25 -0400

    src: added button roll

--- api-dice-app/src/App.js
+++ api-dice-app/src/App.js
@@ -3,32 +3,26 @@ import { fetchRollNumber } from './DiceRollApi';
 import './App.css';
 
 function App() {
-  const [rollNumber, setRollNumber] = useState(null);
+    const [rollNumber, setRollNumber] = useState(null);
 
-  useEffect(() => {
-    const fetchRoll = async () => {
-      const number = await fetchRollNumber();
-      console.log(number)
-      setRollNumber(number);
+    const handleRollClick = async () => {
+        const number = await fetchRollNumber();
+        setRollNumber(number);
     };
 
-    fetchRoll();
-  }, []);
-
-  return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Roll Dice</h1>
-          {rollNumber !== null ? (
-              <p>Your rolled number: {rollNumber}</p>
-          ) : (
-              <p>Loading...</p>
-          )}
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Roll Number App</h1>
+                {rollNumber !== null ? (
+                    <p>Your rolled number: {rollNumber}</p>
+                ) : (
+                    <p>No number rolled yet.</p>
+                )}
+                <button onClick={handleRollClick}>Roll the Number</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
-
-
